0df25ecabfbbe653116b96199ac69db309ec34e5
David Yoon <qqpo12@naver.com>
2026-03-11 07:32:21 +0900

    Dart Studying
    * Dart 학습을 위한 세팅
    * 상수, if 문법, 삼항, 옵셔널 타입 확인

---
+++ bin/cli.dart
@@ -0,0 +1,28 @@
+import 'package:cli/cli.dart' as cli;
+
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    print('Search command recognized!');
+
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+void searchWikipedia(List<String>? arguments) {
+  print('searchWikipedia received arguments: $arguments');
+}
